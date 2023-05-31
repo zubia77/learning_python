73336a0ab760f20b34996586b9d90cfd1c02e2d8
Zubia Rashid <rashzubia@gmail.com>
2023-05-31 10:55:50 +0200

    string format

--- string_format.py
+++ string_format.py
@@ -2,8 +2,12 @@ num1 = 3.1425467389
 num2 = 10.2903948
 
 # previous
-#print("num 1 is", num1, "and num 2 ist", num2)
+print("num 1 is", num1, "and num 2 ist", num2)
 
 # Format Method
 print("num 1 is {0:.3f} and num 2 is {1:.3f}".format(num1, num2))
-# :.3f heisst, dass die Zahl auf 3 Stellen nach dem Komma gekürzt wird. (f steht für float, d..h Kommazahl. lässt man das f weg, werden 3 Zahlen angezeigt.
+# :.3f heisst, dass die Zahl auf 3 Stellen nach dem Komma gekürzt wird. (f steht für float,
+# d..h Kommazahl. lässt man # das f weg, werden 3 Zahlen angezeigt.
+
+# Using F-Strings
+print(f"num 1 is {num1:.4f} and num 2 is {num2:.4f}")
