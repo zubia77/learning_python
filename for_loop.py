8f159da01c419baea9124341860e5436d5f71971
Zubia Rashid <rashzubia@gmail.com>
2023-05-31 11:49:09 +0200

    if statements and for loops

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+kids = ["Abu", "Ehsan", "Ania", "Ayaan"]
+
+# for kid in kids:
+#     print(kid)
+#
+# for kid in kids[1:3]:
+#     print(kid)
+
+for kid in kids:
+    if kid == "Ania":
+        print(f"{kid} - is my daughter")
+        # break
+    else:
+        print(kid)
---
+++ if_statements.py
@@ -0,0 +1,15 @@
+# meaty = input("Do you eat meat? (y/n):")
+#
+# if meaty == "y":
+#     print("Here is the meaty menu...")
+# else:
+#     print("here is the veggie menu...")
+
+
+age= int(input("Enter your age:"))
+if age < 10:
+    print("you are young")
+elif age < 40:
+    print("the fire in you is strong")
+else:
+    print("you are wise beyond doubt")
